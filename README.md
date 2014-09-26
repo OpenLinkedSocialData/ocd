6018a743cecc9b57634fd423c79f767e03e90253
Renato Fabbri <renato.fabbri@gmail.com>
2014-09-26 04:25:01 -0300

    terminada triplificacao de comentarios (terminada parte principal).

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ Os recursos disponibilizados são, principalmente:
 * Dados triplificados do portal: dadosCD.rdf
 * Ontologia do portal: OCD.owl
 
+## Anotações
+* Há dois ids no user\_dados que não encontramos users: '168', '16657'
+* Datas de criação e update não batem do users e do user\_dados. Usado do users
+* A URI dada ao Participante é feita com a ID da tabela users. Uma slug seria melhor, mas não parece ser usada para usuario na plataforma.
+* É bom verificar se há URLs padrão para as postagens e para os usuários. Registrar isso talvez como a URI.
--- triplificaCD.py
+++ triplificaCD.py
@@ -3,6 +3,7 @@
 import rdflib as r
 import _mysql
 import time
+from dateutil.parser import parse
 T=time.time()
 db=_mysql.connect(user="root",passwd="foobar",db="cd")
 db.query("SET NAMES 'utf8'")
@@ -23,3 +24,152 @@ for tt in tables:
     res=db.store_result()
     d[tt]=[res.fetch_row()[0] for i in xrange(res.num_rows())]
 
+g = r.Graph()
+g.namespace_manager.bind("xsd", r.namespace.XSD)    
+g.namespace_manager.bind("ocd", "http://purl.org/socialparticipation/ocd/")    
+rdf = r.namespace.RDF
+ocd = r.Namespace("http://purl.org/socialparticipation/ocd/")
+xsd = r.namespace.XSD
+
+# triplifica users
+ocdp=ocd.User
+du={}
+for user in d["users"]:
+    uid=user[0]
+    email=user[3]
+    created=user[6]
+    updated=user[7]
+    state=user[12]
+    deleted=user[13]
+    ttype=user[14]
+    du[uid]=(email,created,updated)
+
+    uri=ocdp+"#"+uid
+    g.add((uri,rdf.type,ocdp))
+    g.add((uri,ocd.mbox,r.URIRef("mailto:%s"%(email,))))
+    g.add((uri,ocd.created,r.Literal(parse(created))))
+    if updated != created:
+        g.add((uri,ocd.updated,r.Literal(parse(updated))))
+    g.add((uri,ocd.status,r.Literal(state)))
+    if deleted:
+        g.add((uri,ocd.deleted,r.Literal(parse(deleted))))
+    g.add((uri,ocd.type,r.Literal(ttype)))
+foo=[]
+contac=0
+contau=0
+for userd in d["user_dados"]:
+    uid=userd[1]
+    nome=userd[2]
+    fone=userd[3]
+    # email=userd[4] priorizado o email da tabela users
+    site=userd[5]
+    desc=userd[6]
+    gen=userd[7]
+    ani=userd[8]
+    fax=userd[9]
+    created=userd[10]
+    updated=userd[11]
+
+    uri=ocdp+"#"+uid
+    g.add((uri,ocd.name,r.Literal(nome)))
+    g.add((uri,ocd.phone,r.Literal(fone)))
+#    try:
+#        if du[uid][0]!=email:
+#            print "diferente"
+#            print "1: %s, 2: %s"%(du[uid][0],email)
+#    except:
+#        print "usuario existe no user_dados mas nao no users"
+#        foo.append(uid)
+    g.add((uri,ocd.site,r.Literal(site)))
+    g.add((uri,ocd.userDescription,r.Literal(desc)))
+    g.add((uri,ocd.gender,r.Literal(gen)))
+    if ani:
+        g.add((uri,ocd.birthday,r.Literal(parse(ani))))
+    g.add((uri,ocd.fax,r.Literal(fax)))
+    try:
+        if created==du[uid][1]:
+            print "criado certin"
+        else:
+            contac+=1
+            print "criacao n bate"
+        if updated==du[uid][2]:
+            print "updated certin"
+        else:
+            contau+=1
+            print "update n bate"
+    except:
+        print uid
+
+ocdpt=ocd.Post
+ocdc=ocd.Competition
+# posts
+def G(S,P,O):
+    g.add((S,P,O))
+dp={}
+for topico in d["topicos"]:
+    # ttype,uid,titulo,desc,slug,created,updated,ccomment, cadesoes,relevancia,cseguidores,competition_id=topico[1:5]+topico[7:14]+topico[15:]
+    ttype=topico[1] # ["Proposta" ou "Problema"] # OK
+    # Criadas classes para Proposta e problema TTM
+    # Para os especialistas qualificarem
+    uid=topico[2] # co+2 'user_id', parecemments
+    titulo=topico[3] # OK
+    desc=topico[4] # OK
+    slug=topico[7]  # OK
+    created=topico[8] # OK
+    updated=topico[9] # OK
+    ccomment=topico[10] # OK
+    cadesoes=topico[11] # OK
+    relevancia=topico[12] # OK
+    cseguidores=topico[13] # OK
+    competition_id=topico[15] # OK
+
+    uri=ocdpt+"#"+slug
+    dp[topico[0]]=uri
+    G(uri,rdf.type,ocdt)
+    if ttype=="Proposta":
+        G(uri,rdf.type,ocd.Proposal)
+    elif ttype=="Problema":
+        G(uri,rdf.type,ocd.Problem)
+    else:
+        print u"tipo de postagem não identificada"
+    G(uri,ocd.title,r.Literal(titulo))
+    G(uri,ocd.articleBody,r.Literal(desc))
+    G(uri,ocd.created,r.Literal(parse(created)))
+    if updated != created:
+        G(uri,ocd.updated,r.Literal(parse(updated)))
+    G(uri,ocd.commentCount, r.Literal(ccomments))
+    G(uri,ocd.supportCount,r.Literal(cadesoes))
+    G(uri,ocd.relevance,r.Literal(relevancia))
+    G(uri,ocd.followersCount,r.Literal(cseguidores))
+    if competition_id: # Vínculo com Competition
+        uri_=ocdc+"#"+competition_id
+        G(uri,ocd.competition,uri_)
+    
+# Tabela Comments
+for comment in d["comments"]:
+    cid=comment[0] # Ok.
+    tid=comment[1] # opis eh tudo topic
+    body=comment[3] # OK.
+    uid=comment[4] # OK.
+    ttype=comment[8] # Ok.
+    created=comment[9] # OK.
+    updated=comment[10] # Ok.
+    uri=ocd.Comment+"#"+cid
+    G(uri,rdf.type,ocd.Comment)
+    if body:
+        G(uri,ocd.commentBody,body)
+    uri_u=ocdp+"#"+uid
+    G(uri,ocd.author,uri_u)
+    G(uri,ocd.commentType,r.Literal(ttype))
+    G(uri,ocd.created,r.Literal(parse(created)))
+    if updated!=created:
+        G(uri,ocd.updated,r.Literal(parse(updated)))
+    
+
+    
+    
+# Tabela Competitions    
+    
+
+    
+# taggings
